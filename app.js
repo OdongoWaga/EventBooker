00c9a5bc753a28e130aee8e7d08e56f4d375dfc8
Waga Odongo <wagaodongo@gmail.com>
2019-02-18 17:02:44 +0300

    Root Query and Root Mutation creation
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const bodyParser =require('body-parser');
+const graphqlHttp =require('express-graphql');
+const {buildSchema} = require('graphql');
 
 const app =express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) =>{
-    res.send('Hello World');
-} )
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(
+        //Not nullable, cannot be a list of nulls, returns an empty list but cannot return null
+        `
+        type RootQuery {
+            events: [String!]!
+
+        }
+        type RootMutation {
+
+        }
+        
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+        `
+    ),
+    rootValue: {}
+}));
 
 app.listen(3000);
