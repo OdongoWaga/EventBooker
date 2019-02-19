18c4eee61298195ec3cdc39218b5b43c3175fc15
Waga Odongo <wagaodongo@gmail.com>
2019-02-19 12:57:06 +0300

    finished created event
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- app.js
+++ app.js
@@ -98,24 +98,42 @@ app.use('/graphql', graphqlHttp({
                 title:args.eventInput.title,
                 description:args.eventInput.description,
                 price: +args.eventInput.price,
-                date: new Date(args.eventInput.date)
+                date: new Date(args.eventInput.date),
+                creator: '5c6bceb897733408aca8a a4b'
 
             });
+            let createdEvent;
+
              return event
             .save()
             .then(
             result => {
-                console.log(result);
-                return {...result._doc, _id:result._doc._id.toString()};
-            }).catch(err=> {
+                createdEvent=  {...result._doc, _id:result._doc._id.toString()}
+                return User.findById('5c6bceb897733408aca8aa4b')
+                ;
+            }).then(user => {
+                if(user){
+                    throw new Error("User Already Exists")
+                }
+                user.createdEvents.push(event);
+                return user.save();
+            })
+            .then(result => {
+                return createdEvent
+            })
+            .catch(err=> {
             console.log(err);
             throw err;
             });
         },
 
         createUser: args => {
-
-            return bcrypt.hash(args.userInput.password,12)
+             return User.findOne({email:args.userInput.email}).then(user => {
+                if(user){
+                    throw new Error('User Already Exists')
+                }
+                return bcrypt.hash(args.userInput.password,12);
+            }) 
             .then(hashedPassword =>{
                 const user = new User({
                     email:args.userInput.email,
@@ -125,7 +143,8 @@ app.use('/graphql', graphqlHttp({
                 return user.save();
             })
             .then(result => {
-                return {...result._doc, _id:result.id}
+                //set password to null so that it is never sent back to the front end
+                return {...result._doc, password:null, _id:result.id}
             })
             .catch(err =>{
                 throw err;
