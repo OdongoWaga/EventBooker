824a4807b233ffbf47493c68e791abdcdf087bf9
Waga Odongo <wagaodongo@gmail.com>
2019-02-20 17:09:56 +0300

    Auth styling and logic
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route, Redirect,Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
+import AuthPage from './pages/Auth';
+import BookingsPage from './pages/Bookings';
+import EventsPage from './pages/Events';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 import './App.css';
-import Auth from './pages/Auth';
-import Bookings from './pages/Bookings';
-import Events from './pages/Events';
-import Navigation from './components/Navigation/Navigation';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <React.Fragment>
-      <Navigation />
-
-      <main className="main-content"> 
-      <Switch>
-       <Redirect from="/" to ="/auth" exact />
-       <Route path="/auth" component={Auth} />
-       <Route path="/events" component={Events} />
-       <Route path="/bookings" component={Bookings} />
-      </Switch>
-      </main>
-      </React.Fragment>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/events" component={EventsPage} />
+              <Route path="/bookings" component={BookingsPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
       </BrowserRouter>
     );
   }
