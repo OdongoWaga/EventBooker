a6e42092cab34245801bd8066a0472efbf148dd5
Waga Odongo <wagaodongo@gmail.com>
2019-02-21 16:16:32 +0300

    eventitem
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- client/src/components/Events/EventList/EventItem/EventItem.js
+++ client/src/components/Events/EventList/EventItem/EventItem.js
@@ -3,8 +3,16 @@ import './EventItem.css';
 
 const eventItem = props => (
     <li key={props._id} className="events__list-item">
-    {props.title}
+    <div>
+        <h1>{props.title}</h1>
+        <h2> Sh{props.price} - {new Date(props.date).toLocaleDateString()}</h2> 
+    </div>
+    <div>
+       {props.userId===props.creatorId ? (<p> This is your event </p>) : (<button>View Details </button>) }
+       
+    </div>
+
     </li>
 )
 
-export default eventItem;
+export default eventItem; 
--- client/src/components/Events/EventList/EventList.js
+++ client/src/components/Events/EventList/EventList.js
@@ -6,9 +6,15 @@ import './EventList.css';
 const eventList = props => {
 const events= props.events.map(event => {
     return(
-        <li key={event._id} className="events__list-item">
-        {event.title}
-        </li>
+        <EventItem key={event._id} 
+        eventId={event._id} 
+        title={event.title} 
+        price={event.price}
+        date={event.date}
+        userId={props.authUserId}
+        creatorId={event.creator._id}
+        />
+        
     );
 } );
 
